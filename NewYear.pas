466f1b8c37319510f4f616062d5f9bbe89b3763f
Roman Borisenko <reoman.borisenko2016@gmail.com>
2022-10-07 19:38:28 +0300

    Добавлена программа, определяющая какой день ближе к новому году

---
+++ NewYear.pas
@@ -0,0 +1,21 @@
+﻿begin
+  println('Введите номер месяца и число первого дня');
+  var a1 := Readinteger;
+  var b1 := Readinteger;
+  
+  println('Введите номер месяца и число второго дня');
+  var a2 := Readinteger;
+  var b2 := Readinteger;
+  
+  if a1 = a2 then begin
+    if b1 > b2 then
+      print('Первый день ближе к новому году')
+    else if b1 < b2 then
+      print('Второй день ближе к новому году')
+    else print('Это один и тот же день');
+  end
+  else if a1 > a2 then
+      print('Первый день ближе к новому году')
+  else if a1 < a2 then
+    print('Второй день ближе к новому году');
+end.
