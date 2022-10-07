977e554d4fe497aff05ac8d9dccdb14b4d3a4e83
Roman Borisenko <reoman.borisenko2016@gmail.com>
2022-10-07 19:05:17 +0300

    Добавлена программа, проверяющая текущий год на високосность

---
+++ LeapYears.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var a := DateTime.Now.Year;
+  if (a mod 400 = 0) or ((a mod 100 <> 0) and (a mod 4 = 0)) then
+    print($'{a} год високосный')
+  else 
+    print($'{a} год невисокосный')
+end.
